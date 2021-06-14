4340eb12a4af0f355d253c0dcac81563c7d5305a
Fastmore Crak <47199647+FastmoreCrak@users.noreply.github.com>
2021-06-13 22:56:32 -0500

    Add files via upload

---
+++ assistant/start.py
@@ -0,0 +1,198 @@
+﻿# Fastmore - Terminal
+
+from datetime import datetime
+
+from pyUltroid.functions.asst_fns import *
+from pyUltroid.misc._decorators import sed
+from telethon import Button, events
+from telethon.utils import get_display_name
+
+from plugins import *
+
+from . import *
+
+Owner_info_msg = f"""
+**Owner** - {OWNER_NAME}
+**OwnerID** - `{OWNER_ID}`
+
+**Message Forwards** - {udB.get("PMBOT")}
+
+__Ultroid {ultroid_version}, powered by @fastmorecrak__
+"""
+
+
+@asst_cmd("start")
+async def assistant(event):
+    if event.is_group and event.sender_id in sed:
+        bnn = (await asst.get_me()).username
+        return await event.reply(
+            "`I dont work in groups`",
+            buttons=[Button.url("💣INICIAR⚠", url=f"https://t.me/{bnn}?start=set")],
+        )
+    else:
+        if not is_added(event.sender_id) and event.sender_id not in sed:
+            add_user(event.sender_id)
+        ok = ""
+        if event.is_private and event.sender_id in sed:
+            return
+        u = await event.client.get_entity(event.chat_id)
+        if not udB.get("STARTMSG"):
+            if udB.get("PMBOT") == "True":
+                ok = "¡¡Puedes contactar a mi maestro usando este bot !!\n\nEnvíe su mensaje, se lo entregaré al Maestro."
+            await event.reply(
+                f"Hey there [{get_display_name(u)}](tg://user?id={u.id}), this is Ultroid Assistant of [{ultroid_bot.me.first_name}](tg://user?id={ultroid_bot.uid})!\n\n{ok}",
+                buttons=[Button.inline("Info.", data="ownerinfo")],
+            )
+        else:
+            me = f"[{ultroid_bot.me.first_name}](tg://user?id={ultroid_bot.uid})"
+            mention = f"[{get_display_name(u)}](tg://user?id={u.id})"
+            await event.reply(
+                Redis("STARTMSG").format(me=me, mention=mention),
+                buttons=[Button.inline("Info.", data="ownerinfo")],
+            )
+
+
+@callback("ownerinfo")
+async def own(event):
+    await event.edit(Owner_info_msg, buttons=[Button.inline("Close", data="closeit")])
+
+
+@callback("closeit")
+async def closet(lol):
+    await lol.delete()
+
+
+@asst_cmd("start ?(.*)")
+@owner
+async def ultroid(event):
+    if event.pattern_match.group(1):
+        return
+    if event.is_group:
+        return
+    name = event.sender.first_name
+    if event.sender.last_name:
+        name += f" {event.sender.last_name}"
+    await asst.send_message(
+        event.chat_id,
+        get_string("ast_3").format(name),
+        buttons=[
+            [
+                Button.inline("LENGUAJE 🌐", data="lang"),
+                Button.inline("CONFIGURA🔧", data="setter"),
+            ],
+            [
+                Button.inline("STATUS💣", data="stat"),
+                Button.inline("TRASMICION 🔊", data="bcast"),
+            ],
+        ],
+    )
+
+
+# aah, repeat the codes..
+@callback("mainmenu")
+@owner
+async def ultroid(event):
+    if event.is_group:
+        return
+    await event.edit(
+        get_string("ast_3").format(OWNER_NAME),
+        buttons=[
+            [
+                Button.inline("LENGUAJE 🌐", data="lang"),
+                Button.inline("CONFIGURA🔧", data="setter"),
+            ],
+            [
+                Button.inline("STATUS💣", data="stat"),
+                Button.inline("TRASMICION🔊", data="bcast"),
+            ],
+        ],
+    )
+
+
+@callback("stat")
+@owner
+async def botstat(event):
+    ok = len(get_all_users())
+    msg = """Fastmore Assistant - Stats
+Total Users - {}""".format(
+        ok,
+    )
+    await event.answer(msg, cache_time=0, alert=True)
+
+
+@callback("bcast")
+@owner
+async def bdcast(event):
+    ok = get_all_users()
+    await event.edit(f"Broadcast to {len(ok)} users.")
+    async with event.client.conversation(OWNER_ID) as conv:
+        await conv.send_message(
+            "Enter your broadcast message.\nUse /cancel to stop the broadcast.",
+        )
+        response = conv.wait_event(events.NewMessage(chats=OWNER_ID))
+        response = await response
+        themssg = response.message.message
+        if themssg == "/cancel":
+            return await conv.send_message("Cancelled!!")
+        else:
+            success = 0
+            fail = 0
+            await conv.send_message(f"Starting a broadcast to {len(ok)} users...")
+            start = datetime.now()
+            for i in ok:
+                try:
+                    await asst.send_message(int(i), f"{themssg}")
+                    success += 1
+                except BaseException:
+                    fail += 1
+            end = datetime.now()
+            time_taken = (end - start).seconds
+            await conv.send_message(
+                f"""
+Broadcast completed in {time_taken} seconds.
+Total Users in Bot - {len(ok)}
+Sent to {success} users.
+Failed for {fail} user(s).""",
+            )
+
+
+@callback("setter")
+@owner
+async def setting(event):
+    await event.edit(
+        "Choose from the below options -",
+        buttons=[
+            [
+                Button.inline("API Kᴇʏs🔑", data="apiset"),
+                Button.inline("Pᴍ Bᴏᴛ🔧", data="chatbot"),
+            ],
+            [
+                Button.inline("Aʟɪᴠᴇ🔴", data="alvcstm"),
+                Button.inline("PᴍPᴇʀᴍɪᴛ🗄", data="ppmset"),
+            ],
+            [Button.inline("Fᴇᴀᴛᴜʀᴇs📜", data="otvars")],
+            [Button.inline("VC Sᴏɴɢ Bᴏᴛ🎧", data="vcb")],
+            [Button.inline("❌Bᴀᴄᴋ❌", data="mainmenu")],
+        ],
+    )
+
+
+@asst_cmd("start set")
+@owner
+async def set(event):
+    await event.reply(
+        "Choose from the below options -",
+        buttons=[
+            [
+                Button.inline("API Kᴇʏs🔑", data="apiset"),
+                Button.inline("Pᴍ Bᴏᴛ🔧", data="chatbot"),
+            ],
+            [
+                Button.inline("Aʟɪᴠᴇ🔴", data="alvcstm"),
+                Button.inline("PᴍPᴇʀᴍɪᴛ🗄", data="ppmset"),
+            ],
+            [Button.inline("Fᴇᴀᴛᴜʀᴇs📜", data="otvars")],
+            [Button.inline("VC Sᴏɴɢ Bᴏᴛ🎧", data="vcb")],
+            [Button.inline("❌Bᴀᴄᴋ❌", data="mainmenu")],
+        ],
+    )
---
+++ resources/session/ssgen.py
@@ -0,0 +1,76 @@
+#!/bin/bash
+# fastmore - terminal
+
+import os
+from time import sleep
+
+# https://www.tutorialspoint.com/how-to-clear-screen-in-python#:~:text=In%20Python%20sometimes%20we%20have,screen%20by%20pressing%20Control%20%2B%20l%20.
+if os.name == "posix":
+    _ = os.system("clear")
+else:
+    # for windows platfrom
+    _ = os.system("cls")
+
+a = r"""
+Fastmore Crak
+"""
+
+print(a)
+try:
+    print("Checking if Telethon is installed...")
+
+    for x in range(3):
+        for frame in r"-\|/-\|/":
+            print("\b", frame, sep="", end="", flush=True)
+            sleep(0.1)
+    import telethon
+    x = "\bFound an existing installation of Telethon...\nSuccessfully Imported.\n\n"
+except BaseException:
+    print("Installing Telethon...")
+    os.system("pip install telethon")
+
+    x = "\bDone. Installed and imported Telethon."
+if os.name == "posix":
+    _ = os.system("clear")
+else:
+    # for windows platfrom
+    _ = os.system("cls")
+print(a)
+print(x)
+
+# the imports
+
+from telethon.errors.rpcerrorlist import ApiIdInvalidError, PhoneNumberInvalidError
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
+print(
+    "Get your API ID and API HASH from my.telegram.org or @ScrapperRoBot to proceed.\n\n",
+)
+
+try:
+    API_ID = int(input("Please enter your API ID: "))
+except ValueError:
+    print("APP ID must be an integer.\nQuitting...")
+    exit(0)
+API_HASH = input("Please enter your API HASH: ")
+
+# logging in
+try:
+    with TelegramClient(StringSession(), API_ID, API_HASH) as ultroid:
+        print("Generating a user session for Fastmore Crak...")
+        ult = ultroid.send_message(
+            "me",
+            f"**ULTROID** `SESSION`:\n\n`{ultroid.session.save()}`\n\n**Do not share this anywhere!**",
+        )
+        print("Your SESSION has been generated. Check your telegram saved messages!")
+        exit(0)
+except ApiIdInvalidError:
+    print("Your API ID/API HASH combination is invalid. Kindly recheck.\nQuitting...")
+    exit(0)
+except ValueError:
+    print("API HASH must not be empty!\nQuitting...")
+    exit(0)
+except PhoneNumberInvalidError:
+    print("The phone number is invalid!\nQuitting...")
+    exit(0)
